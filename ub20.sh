396f354be37294512d174e2c3b66c0d96c1dc2e5
Rega23 <92116230+Rega23@users.noreply.github.com>
2023-02-09 07:15:42 +0700

    Update ub20.sh

--- ub20.sh
+++ ub20.sh
@@ -200,7 +200,7 @@ RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_r
     /root/.acme.sh/acme.sh --issue -d $SUB_DOMAIN --standalone -k ec-256 >/dev/null 2>&1
     ~/.acme.sh/acme.sh --installcert -d $SUB_DOMAIN --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key --ecc >/dev/null 2>&1
     judge "Installed slowdns"
-    wget -q -O /etc/nameserver https://github.com/Rega23/mrg/main/X-SlowDNS/nameserver" && bash /etc/nameserver >/dev/null 2>&1
+    wget -q -O /etc/nameserver https://github.com/Rega23/mrg/main/X-SlowDNS/nameserver && bash /etc/nameserver >/dev/null 2>&1
     
 #nginx_install
         sudo apt-get update -y >/dev/null 2>&1
